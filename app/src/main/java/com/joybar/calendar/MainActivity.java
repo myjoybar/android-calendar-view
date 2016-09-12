f0fcba6ad9dc1fe1a7db5c88fc62341c50c8c414
myjoybar <myjoybar@gmail.com>
2016-09-12 21:02:22 +0800

    优化代码

--- app/src/main/java/com/joybar/calendar/MainActivity.java
+++ app/src/main/java/com/joybar/calendar/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.joybar.librarycalendar.data.CalendarDate;
@@ -19,7 +21,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements
         CalendarViewPagerFragment.OnPageChangeListener,
         CalendarViewFragment.OnDateClickListener,
-        CalendarViewFragment.OnDateCancelListener{
+        CalendarViewFragment.OnDateCancelListener {
 
     private TextView tv_date;
     private boolean isChoiceModelSingle = false;
@@ -30,51 +32,79 @@ public class MainActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv_date = (TextView) findViewById(R.id.tv_date);
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction tx = fm.beginTransaction();
-       // Fragment fragment = new CalendarViewPagerFragment();
-        Fragment fragment =  CalendarViewPagerFragment.newInstance(isChoiceModelSingle);
-        tx.add(R.id.fl_content, fragment);
-        tx.commit();
+        initFragment();
     }
 
+   private void initFragment(){
+       FragmentManager fm = getSupportFragmentManager();
+       FragmentTransaction tx = fm.beginTransaction();
+       // Fragment fragment = new CalendarViewPagerFragment();
+       Fragment fragment = CalendarViewPagerFragment.newInstance(isChoiceModelSingle);
+       tx.replace(R.id.fl_content, fragment);
+       tx.commit();
+   }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_im, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_single:
+                isChoiceModelSingle = true;
+                initFragment();
+                break;
+            case R.id.menu_multi:
+                isChoiceModelSingle = false;
+                initFragment();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
     @Override
     public void OnDateClick(CalendarDate calendarDate) {
 
         int year = calendarDate.getSolar().solarYear;
         int month = calendarDate.getSolar().solarMonth;
         int day = calendarDate.getSolar().solarDay;
-        if(isChoiceModelSingle){
+        if (isChoiceModelSingle) {
             tv_date.setText(year + "-" + month + "-" + day);
-        }else{
-            System.out.println(calendarDate.getSolar().solarDay);
+        } else {
+            //System.out.println(calendarDate.getSolar().solarDay);
             mListDate.add(calendarDate);
             tv_date.setText(listToString(mListDate));
         }
 
     }
+
     @Override
     public void OnDateCancel(CalendarDate calendarDate) {
-     int count = mListDate.size();
-        for(int i = 0;i<count;i++){
+        int count = mListDate.size();
+        for (int i = 0; i < count; i++) {
             CalendarDate date = mListDate.get(i);
-            if(date.getSolar().solarDay==calendarDate.getSolar().solarDay){
+            if (date.getSolar().solarDay == calendarDate.getSolar().solarDay) {
                 mListDate.remove(i);
                 break;
             }
         }
         tv_date.setText(listToString(mListDate));
     }
+
     @Override
     public void OnPageChange(int year, int month) {
         tv_date.setText(year + "-" + month);
         mListDate.clear();
     }
 
-    private static String listToString(List<CalendarDate>list){
+    private static String listToString(List<CalendarDate> list) {
         StringBuffer stringBuffer = new StringBuffer();
-        for(CalendarDate  date  : list)    {
-            stringBuffer.append(date.getSolar().solarYear+"-"+date.getSolar().solarMonth+"-"+date.getSolar().solarDay).append(" ");
+        for (CalendarDate date : list) {
+            stringBuffer.append(date.getSolar().solarYear + "-" + date.getSolar().solarMonth + "-" + date.getSolar().solarDay).append(" ");
         }
         return stringBuffer.toString();
     }
